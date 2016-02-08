20eecab7c90c1b3398df548ed51a3d956e7ecbae
Julio Reyes <nemesiscodex@gmail.com>
2016-02-08 03:07:41 -0300

    request header parser microservice

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var path = process.cwd();
-
 module.exports = function(app) {
     app.route('/')
         .get(function(req, res) {
-            res.sendFile(path + '/public/index.html');
+            var userAgent = req.get('user-agent');
+            var obj = {
+                ip: req.headers['x-forwarded-for'] ||
+					req.connection.remoteAddress,
+                userAgent: userAgent.substring(userAgent.indexOf('(') + 1,
+                    userAgent.indexOf(')')),
+                lang: req.acceptsLanguages()[0]
+            };
+
+            res.type("application/json");
+            res.send(obj);
         });
-    
 };
